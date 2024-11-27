0950cc4a573900b1044e9a3bfe5fce41679c5d6e
Vitor Soares <vitorratollasoares@gmail.com>
2024-11-27 11:25:21 -0300

    First Commit

---
+++ twitter_simplified/src/main/java/com/ratolla/twitter_simplified/TwitterSimplifiedApplication.java
@@ -0,0 +1,13 @@
+package com.ratolla.twitter_simplified;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterSimplifiedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterSimplifiedApplication.class, args);
+	}
+
+}
---
+++ twitter_simplified/src/test/java/com/ratolla/twitter_simplified/TwitterSimplifiedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ratolla.twitter_simplified;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterSimplifiedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
