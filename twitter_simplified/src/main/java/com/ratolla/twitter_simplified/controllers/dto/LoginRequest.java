37787dcbd84cec97305c26346b142fbfb4308e0e
Vitor Soares <vitorratollasoares@gmail.com>
2024-11-28 12:44:24 -0300

    Keys + DTO

---
+++ twitter_simplified/src/main/java/com/ratolla/twitter_simplified/controllers/dto/LoginRequest.java
@@ -0,0 +1,4 @@
+package com.ratolla.twitter_simplified.controllers.dto;
+
+public record LoginRequest(String username, String password) {
+}
---
+++ twitter_simplified/src/main/java/com/ratolla/twitter_simplified/controllers/dto/LoginResponse.java
@@ -0,0 +1,4 @@
+package com.ratolla.twitter_simplified.controllers.dto;
+
+public record LoginResponse(String accessToken, Long expiresIn) {
+}
